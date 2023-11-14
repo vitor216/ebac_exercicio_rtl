1aba249b2377850561a6c2f1a35e5578adbdce28
vitor <eletrotecvitor@gmail.com>
2023-11-14 18:15:12 -0300

    teste para erificar dois comentarios

--- src/components/Post/index.tsx
+++ src/components/Post/index.tsx
@@ -10,7 +10,7 @@ type Props = {
 
 const Post = ({ children, imageUrl }: Props) => (
     <div className={styles.post}>
-        <img className={styles['post-image']} src={imageUrl} />
+        <img className={styles['post-image']} src={imageUrl} alt=''/>
         <p className={styles['post-text']}> {children} </p>
         <PostComments />
     </div>
--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -1,10 +1,28 @@
 import { fireEvent, render, screen } from '@testing-library/react';
-import Post from '.';
 import PostComment from '.';
 
 describe('Teste para o componente PostComment', () => {
     it('Deve renderizar o componente corretamente', () => {
         render(<PostComment/>);
         expect(screen.getByText('Comentar')).toBeInTheDocument();
+    });
+
+    it('verifica se foi feito 2 comentarios', () => {
+        render(<PostComment/>);
+        fireEvent.change(screen.getByTestId('coment-textarea'), {
+            target: {
+                value: 'Primeiro comentário',
+            }
+        });
+        fireEvent.click(screen.getByTestId('coment-button'));
+
+        fireEvent.change(screen.getByTestId('coment-textarea'), {
+            target: {
+                value: 'Segundo comentário',
+            }
+        });
+        fireEvent.click(screen.getByTestId('coment-button'));
+
+        expect(screen.getAllByTestId('coment-element')).toHaveLength(2);
     });
 });
